2e550ec93cd008a15fdd382b21b3c6ffae0d76a3
Steve Au <steve@steveau.com>
2021-06-03 21:22:38 -0700

    Add "carousel" support to handle multiple image sources

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # MMM-ProxyImage
 - Load basic auth security camera images into your Magic Mirror.
 - Adds an Express endpoint that the DOM renderer uses as an image source.
+- Supports multiple image sources that can be "carousel" cycled.
 - Bypasses the deprecation of [embedded credentials in subresource requests](https://www.chromestatus.com/feature/5669008342777856) where basic auth credentials are ignored in asset requests.
 
 ## Screenshot
@@ -21,13 +22,18 @@ git clone https://github.com/daxiang28/MMM-ProxyImage
   position: "top_left",
   config: {
     name: 'FrontDoor', // Must be unique if using multiple instances
-    updateInterval : 4000,
+    updateInterval : 4000, // Duration between image refresh
+    slideInterval: 10000, // Optional: Duration of each slide when multiple hosts are provided
     host: 'http://[CAMERA_IP]/[PATH_TO_STATIC_IMAGE]',
+    // Alternately, use an array to cycle through sources 
+    // Use the `slideInterval` to set the duration of each slide
+    // `user` and `pass` configs are ignored if an array is used
+    host: ['http://[AUTH_USER]:[AUTH_PASS]@[CAMERA_IP]/[PATH_TO_STATIC_IMAGE]'],
     port: 80,
     height: 400, // Pixel height of the image
     width: 400, // Pixel width of the image
-    user: '[USERNAME]',
-    pass: '[PASSWORD]'
+    user: '[USERNAME]', // Ignored if a host array is defined
+    pass: '[PASSWORD]' // Ignored if a host array is defined
   }
 }
 ```
--- node_helper.js
+++ node_helper.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const NodeHelper = require("node_helper")
+const url = require('url');
 
 module.exports = NodeHelper.create({
 	start: function() {
@@ -9,17 +10,40 @@ module.exports = NodeHelper.create({
 	socketNotificationReceived: function(notification, payload) {
 		if (notification == "INIT") {
 	  		this.config[payload.instanceName] = payload;
+			if (payload.slideInterval) {
+				// Instance specific vars
+				this.config[payload.instanceName]['cycles'] = Math.floor(payload.slideInterval / payload.updateInterval);
+				this.config[payload.instanceName]['lastHostIndex'] = 0
+				this.config[payload.instanceName]['cycleCount'] = 0
+			}
 	  		this.webserver();
 		}
   	},
 
 	webserver: function() {
 		this.expressApp.get(`/${this.name}/:config`, async (req, res) => {
-			const config = this.config[req.params.config];
-			const response = await axios.get(config.host, {
+			let { host, user, pass, lastHostIndex, cycles, cycleCount } = this.config[req.params.config];
+			
+			if (typeof host === 'object') {
+				let nextIndex = lastHostIndex;				
+				this.config[req.params.config].cycleCount = cycleCount + 1;
+
+				if (cycleCount >= cycles) {
+					nextIndex = (lastHostIndex + 1 >= host.length) ? 0 : lastHostIndex + 1;
+					this.config[req.params.config].cycleCount = 0;
+				}
+
+				const { auth, href } = url.parse(host[nextIndex]);
+				host = href;
+				[user, pass] = auth.split(':');
+
+				this.config[req.params.config].lastHostIndex = nextIndex;
+			}
+
+			const response = await axios.get(host, {
 				auth: {
-					username: config.user,
-					password: config.pass
+					username: user,
+					password: pass
 				},
 				responseType: 'arraybuffer'
 			});
