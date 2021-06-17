151965e0d7f684331a308a0bc876ee01298f61dc
Steve Au <steve@steveau.com>
2021-06-17 12:32:26 -0700

    Adding linting

--- MMM-ProxyImage.js
+++ MMM-ProxyImage.js
@@ -1,142 +1,151 @@
+// eslint-disable-next-line no-undef
+Module.register('MMM-ProxyImage', {
+  defaults: {
+    modalEnabled: false,
+    modalInterval: null,
+    modalTemplate: 'modal_template.njk',
+    updateInterval: 4000,
+    slideInterval: 10000,
+    port: 80,
+    height: 400,
+    width: 400,
+    auth: 'basic' // basic|digest
+  },
 
-Module.register("MMM-ProxyImage",{
-	defaults: { 
-		modalEnabled: false,
-		modalInterval: null,
-		modalTemplate: 'modal_template.njk',
-		updateInterval: 4000,
-		slideInterval: 10000,
-		port: 80,
-		height: 400,
-		width: 400,
-		auth: 'basic' // basic|digest
-	},
+  start() {
+    // Allows for multiple instances of the module
+    this.config.instanceName = (this.config.name || this.name).replace(
+      /\s+/g,
+      ''
+    );
 
-	start: function() {
-		// Allows for multiple instances of the module
-		this.config.instanceName = (this.config.name || this.name).replace(/\s+/g, '');
-		let self = this;
+    // send config to node helper
+    this.sendSocketNotification('INIT', this.config);
 
-		// send config to node helper
-		this.sendSocketNotification("INIT", this.config)
+    // Schedule update timer.
+    this.scheduleUpdate(this.config.updateInterval);
+  },
 
-		// Schedule update timer.
-		this.scheduleUpdate(this.config.updateInterval);
-	},
+  scheduleUpdate(delay) {
+    const self = this;
+    let nextLoad = self.config.updateInterval;
+    if (typeof delay !== 'undefined' && delay >= 0) {
+      nextLoad = delay;
+    }
+    setTimeout(() => {
+      self.updateDom();
+      self.scheduleUpdate();
+    }, nextLoad);
+  },
 
-	scheduleUpdate: function(delay) {
-		let self = this;
-		let nextLoad = self.config.updateInterval;
-		if (typeof delay !== "undefined" && delay >= 0) {
-			nextLoad = delay;
-		}
-		setTimeout(function() {
-			self.updateDom();
-			self.scheduleUpdate();
-		}, nextLoad);
-	},
+  showModal(imageSrc) {
+    const self = this;
+    const { modalInterval, updateInterval } = self.config;
+    const refreshInterval = modalInterval || updateInterval;
+    this.sendNotification('OPEN_MODAL', {
+      template: self.config.modalTemplate,
+      data: {
+        imageSrc
+      },
+      options: {
+        callback(error) {
+          if (error) {
+            console.error('Modal rendering failed', error);
+            return false;
+          }
+          const modalTimer = setInterval(() => {
+            const modal = document.querySelector('.MMM-Modal');
+            if (modal.style.opacity === '1') {
+              const img = modal.querySelector('#proxy-image-modal-image');
+              img.src = imageSrc;
+            } else {
+              clearInterval(modalTimer);
+            }
+          }, refreshInterval);
+        }
+      }
+    });
+  },
 
-	showModal: function(imageSrc) {
-		let self = this;
-		const refreshInterval = self.config.modalInterval || self.config.updateInterval;
-		this.sendNotification("OPEN_MODAL", {
-			template: self.config.modalTemplate,
-			data: {
-				imageSrc
-			},
-			options: {
-				callback: function(error) {
-					if (error) {
-						console.error('Modal rendering failed', error);
-						return false;
-					}
-					const modalTimer = setInterval(function() {
-						const modal = document.querySelector('.MMM-Modal');
-						if (modal.style.opacity == 1) {
-							const img = modal.querySelector('#proxy-image-modal-image');
-							img.src = imageSrc;
-						} else {
-							clearInterval(modalTimer);
-						}
-					}, refreshInterval);					
-				}					
-			}		
-		});
-	},
+  getDom() {
+    const self = this;
+    const d = new Date();
+    const wrapperId = `proxyDiv${self.config.instanceName}`;
+    let wrapper = document.getElementById(wrapperId);
+    const imgId = `proxyImage${self.config.instanceName}`;
+    let img = document.getElementById(imgId);
+    const imgId2 = `proxyImage2${self.config.instanceName}`;
+    let img2 = document.getElementById(imgId2);
+    const imageSrc = `/${self.name}/${self.config.instanceName}?${d.getTime()}`;
 
-	getDom: function() {
-		let self = this;
-		const d = new Date();
-		const wrapperId = "proxyDiv"+self.config.instanceName;
-		let wrapper = document.getElementById(wrapperId);
-		const imgId = "proxyImage"+self.config.instanceName;
-		let img = document.getElementById(imgId);
-		const imgId2 = "proxyImage2"+self.config.instanceName;
-		let img2 = document.getElementById(imgId2);
-		const imageSrc = `/${self.name}/${self.config.instanceName}?${d.getTime()}`;
+    if (!wrapper) {
+      wrapper = document.createElement('div');
+      wrapper.id = wrapperId;
+      wrapper.className = 'image-wrapper';
+      wrapper.style.width = `${self.config.width}px`;
+      wrapper.style.height = `${self.config.height}px`;
+      wrapper.style.border = 'none';
+      wrapper.style.display = 'block';
+      wrapper.style.overflow = 'hidden';
+      wrapper.style.backgroundColor = self.config.backgroundColor;
+      wrapper.scrolling = 'no';
+    }
 
-		if (!wrapper) {
-			wrapper = document.createElement("div");
-			wrapper.id = wrapperId;
-			wrapper.className = "image-wrapper"
-			wrapper.style.width = self.config.width + 'px';
-			wrapper.style.height = self.config.height + 'px';
-			wrapper.style.border = "none";
-			wrapper.style.display = "block";
-			wrapper.style.overflow = "hidden";
-			wrapper.style.backgroundColor = self.config.backgroundColor;
-			wrapper.scrolling = "no";		
-		}
+    if (self.config.modalEnabled) {
+      wrapper.onclick = () => {
+        self.showModal(imageSrc);
+      };
+    }
 
-		if (self.config.modalEnabled) {
-			wrapper.onclick = function() { self.showModal(imageSrc) };
-		}
+    if (!img) {
+      img = document.createElement('img');
+      img.id = imgId;
+      img.width = self.config.width;
+      img.src = imageSrc;
+      wrapper.appendChild(img);
 
-		if (!img) {
-			img = document.createElement("img");
-			img.id = imgId;
-			img.width = self.config.width;
-			img.src = imageSrc;
-			wrapper.appendChild(img);
+      img2 = document.createElement('img');
+      img2.id = imgId2;
+      img2.width = self.config.width;
+      img2.style.display = 'none';
+      wrapper.appendChild(img2);
+    } else if (img && img.style.display !== 'none') {
+      // Give them a chance to load before swapping
+      img2.src = imageSrc;
+      setTimeout(() => {
+        img2.style.display = 'block';
+        img.style.display = 'none';
+      }, 2000);
+    } else if (img) {
+      img.src = imageSrc;
+      setTimeout(() => {
+        img.style.display = 'block';
+        img2.style.display = 'none';
+      }, 2000);
+    }
 
-			img2 = document.createElement("img");
-			img2.id = imgId2;
-			img2.width = self.config.width;
-			img2.style.display = 'none';
-			wrapper.appendChild(img2);			
-		} else {
-			// Give them a chance to load before swapping
-			if (img.style.display !== 'none') {
-				img2.src = imageSrc;
-				setTimeout(() => { img2.style.display = 'block';img.style.display = 'none'; }, 2000);				
-			} else {
-				img.src = imageSrc;
-				setTimeout(() => { img.style.display = 'block';img2.style.display = 'none'; }, 2000);
-			}
-		}
+    return wrapper;
+  },
 
-		return wrapper;
-	},
+  suspend() {
+    const doms = document.getElementsByClassName('image-wrapper');
+    if (doms.length > 0) {
+      doms.forEach((dom) => {
+        dom.style.display = 'none';
+      });
+    }
+  },
 
-	suspend: function() {
-		const doms = document.getElementsByClassName("image-wrapper")
-		if (doms.length > 0) {
-			for (let dom of doms) {
-				dom.style.display = "none";
-			}
-		}
-	},
+  resume() {
+    const doms = document.getElementsByClassName('image-wrapper');
+    if (doms.length > 0) {
+      doms.forEach((dom) => {
+        dom.style.display = 'block';
+      });
+    }
+  },
 
-	resume: function() {
-		const doms = document.getElementsByClassName("image-wrapper")
-		if (doms.length > 0) {
-			for (let dom of doms) {
-				dom.style.display = "block";
-			}
-		}
-	},
-
-	getStyles: function() {
-		return ["MMM-ProxyImage.css"];
-	}
+  getStyles() {
+    return ['MMM-ProxyImage.css'];
+  }
 });
--- node_helper.js
+++ node_helper.js
@@ -1,65 +1,81 @@
-const NodeHelper = require("node_helper")
+const NodeHelper = require('node_helper');
 const url = require('url');
 const urllib = require('urllib');
 
 module.exports = NodeHelper.create({
-	start: function() {
-		this.config = {};
-	},
+  start() {
+    this.config = {};
+  },
 
-	socketNotificationReceived: function(notification, payload) {
-		if (notification == "INIT") {
-	  		this.config[payload.instanceName] = payload;
-			if (payload.slideInterval) {
-				// Instance specific vars
-				this.config[payload.instanceName]['cycles'] = Math.floor(payload.slideInterval / payload.updateInterval);
-				this.config[payload.instanceName]['lastHostIndex'] = 0
-				this.config[payload.instanceName]['cycleCount'] = 0
-			}
-	  		this.webserver();
-		}
-  	},
+  socketNotificationReceived(notification, payload) {
+    if (notification === 'INIT') {
+      this.config[payload.instanceName] = payload;
+      if (payload.slideInterval) {
+        // Instance specific vars
+        this.config[payload.instanceName].cycles = Math.floor(
+          payload.slideInterval / payload.updateInterval
+        );
+        this.config[payload.instanceName].lastHostIndex = 0;
+        this.config[payload.instanceName].cycleCount = 0;
+      }
+      this.webserver();
+    }
+  },
 
-	webserver: function() {
-		this.expressApp.get(`/${this.name}/:config`, async (req, res) => {
-			let { host, user, pass, lastHostIndex, cycles, cycleCount, authType } = this.config[req.params.config];
-			
-			if (typeof host === 'object') {
-				let nextIndex = lastHostIndex;				
-				this.config[req.params.config].cycleCount = cycleCount + 1;
+  webserver() {
+    this.expressApp.get(`/${this.name}/:config`, async (req, res) => {
+      const config = this.config[req.params.config];
+      const { lastHostIndex, cycles, cycleCount, authType } = config;
+      let href;
+      let { host, user, pass, port } = config;
 
-				if (cycleCount >= cycles) {
-					nextIndex = (lastHostIndex + 1 >= host.length) ? 0 : lastHostIndex + 1;
-					this.config[req.params.config].cycleCount = 0;
-				}
+      const parseUrl = (h) => {
+        const { auth, href: parsedHref, port: parsedPort } = url.parse(h);
+        let parsedUser;
+        let parsedPass;
+        if (auth) {
+          [parsedUser, parsedPass] = auth.split(':');
+        }
+        return [parsedUser, parsedPass, parsedPort, parsedHref];
+      };
 
-				const { auth, href } = url.parse(host[nextIndex]);
-				host = href;
-				if (auth) {
-					[user, pass] = auth.split(':');
-				}
+      if (typeof host === 'object') {
+        let nextIndex = lastHostIndex;
+        this.config[req.params.config].cycleCount = cycleCount + 1;
 
-				this.config[req.params.config].lastHostIndex = nextIndex;
-			}
+        if (cycleCount >= cycles) {
+          // TODO figure out how to handle index problem underined error
+          nextIndex = lastHostIndex + 1 >= host.length ? 0 : lastHostIndex + 1;
+          this.config[req.params.config].cycleCount = 0;
+        }
+        [user, pass, port, href] = parseUrl(host[nextIndex]);
+        host = href;
+        this.config[req.params.config].lastHostIndex = nextIndex;
+      } else {
+        const [parsedUser, parsedPass, parsedPort] = parseUrl(host);
+        if (!user) user = parsedUser;
+        if (!pass) pass = parsedPass;
+        if (!port) port = parsedPort;
+      }
 
-			try {
-				let authOptions = {};
-				const authKey = authType === 'digest' ? 'digestAuth' : 'auth';
-				if (user && pass) {
-					authOptions[authKey] = `${user}:${pass}`;
-				}
-	
-				const response = await urllib.request(host, {
-					method: 'GET',
-					...authOptions
-				});
+      try {
+        const authOptions = {};
+        const authKey = authType === 'digest' ? 'digestAuth' : 'auth';
+        if (user && pass) {
+          authOptions[authKey] = `${user}:${pass}`;
+        }
 
-				res.set('Content-Type', 'image/jpeg');
-				res.set('Cache-Control', 'no-store');
-				res.end(response.data, 'binary');
-			} catch (e) {
-				res.end(e.message);
-			}
-    	});
-	}
-})
+        const response = await urllib.request(`${host}`, {
+          method: 'GET',
+          ...authOptions
+        });
+
+        res.set('Content-Type', 'image/jpeg');
+        res.set('Cache-Control', 'no-store');
+        res.end(response.data, 'binary');
+      } catch (e) {
+        res.end(e.message);
+      }
+    });
+  }
+});
